27d667e67283e6ca984b081f15f2515a10ee7e8f
Oleksandr-Kalnaus <oleksandrkalnaus@gmail.com>
2024-09-08 12:59:05 +0100

    Edit modal window

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,33 +1,23 @@
-const sendBtn = document.querySelector('.submit-comment-form');
-const modalWindow = document.querySelector('.modal-window');
-const modalOverlay = document.querySelector('.modal-overlay');
-const closeBtn = document.querySelector('.close-modal-btn');
-const form = document.querySelector('form');
+document.addEventListener('DOMContentLoaded', () => {
+    const sendBtn = document.querySelector('.submit-comment-form');
+    const modalWindow = document.querySelector('.modal-window');
+    const modalOverlay = document.querySelector('.modal-overlay');
+    const closeBtn = document.querySelector('.close-modal-btn');
+    const form = document.querySelector('form');
 
-sendBtn.onclick = function(event) {
-    event.preventDefault();
-    
-    modalWindow.style.display = 'block';
-    modalOverlay.style.display = 'block';
-    
-    setTimeout(() => {
-        modalWindow.classList.add('show');
-        modalOverlay.classList.add('show');
-    }, 10);
-}
+    sendBtn.onclick = function(event) {
+        event.preventDefault();
+        
+        modalWindow.style.display = 'block';
+        modalOverlay.style.display = 'block';
+        
+        setTimeout(() => {
+            modalWindow.classList.add('show');
+            modalOverlay.classList.add('show');
+        }, 10);
+    }
 
-closeBtn.onclick = function() {
-    modalWindow.classList.remove('show');
-    modalOverlay.classList.remove('show');
-
-    setTimeout(() => {
-        modalWindow.style.display = 'none';
-        modalOverlay.style.display = 'none';
-    }, 1000);
-}
-
-window.onclick = function(event) {
-    if (event.target == modalOverlay) {
+    closeBtn.onclick = function() {
         modalWindow.classList.remove('show');
         modalOverlay.classList.remove('show');
 
@@ -36,14 +26,26 @@ window.onclick = function(event) {
             modalOverlay.style.display = 'none';
         }, 1000);
     }
-}
 
-closeBtn.addEventListener('click', () => {
-    form.reset();
-});
+    window.onclick = function(event) {
+        if (event.target == modalOverlay) {
+            modalWindow.classList.remove('show');
+            modalOverlay.classList.remove('show');
 
-window.addEventListener('click', (event) => {
-    if (event.target == modalOverlay) {
-        form.reset();
+            setTimeout(() => {
+                modalWindow.style.display = 'none';
+                modalOverlay.style.display = 'none';
+            }, 1000);
+        }
     }
+
+    closeBtn.addEventListener('click', () => {
+        form.reset();
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target == modalOverlay) {
+            form.reset();
+        }
+    });
 });
