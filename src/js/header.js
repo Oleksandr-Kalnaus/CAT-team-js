ffb4297a3e75312d3ffd954d6bf9e71792855482
Oleksandr-Kalnaus <oleksandrkalnaus@gmail.com>
2024-09-11 22:25:53 +0100

    Add header

---
+++ src/js/header.js
@@ -0,0 +1,14 @@
+(() => {
+  const menuBtn = document.querySelector('.js-open-menu');
+  const menu = document.querySelector('.js-menu-container');
+  const body = document.body;
+
+  const toggleMenu = () => {
+    const isMenuOpen = menuBtn.getAttribute('aria-expanded') === 'true';
+    menuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    menu.classList.toggle('is-open');
+    body.classList.toggle('no-scroll', !isMenuOpen);
+  };
+
+  menuBtn.addEventListener('click', toggleMenu);
+})(); 
